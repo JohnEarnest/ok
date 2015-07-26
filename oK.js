876f27567f2b3970165f8e97f1b9ab5911d03e8e
John Earnest <JohnEarnest@gmail.com>
2015-07-26 10:16:45 -0600

    k5 still has special cases for applying over to a zero length list, but only when that list is numeric. Since oK doesn't currently support unitype vectors, I will alter these test cases slightly. Also note that min and max have slightly different (and more sensible, I think) default cases.

--- oK.js
+++ oK.js
@@ -280,7 +280,7 @@ function eachpc(dyad, x, y, env) {
 }
 
 function over(dyad, x, env) {
-	var specials = {"+":k0, "*":k1, "|":k0, "&":k1};
+	var specials = {"+":k0, "*":k1, "|":k(0,-1/0), "&":k(0,1/0)};
 	if (x.t == 3 && len(x) < 1 && dyad.v in specials) { return specials[dyad.v]; }
 	if (x.t != 3 || len(x) < 1) { return x; }
 	return x.v.reduce(function(x, y) { return applyd(dyad, x, y, env); });
