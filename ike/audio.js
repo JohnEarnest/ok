53b96a8dcdd91c241d5446bd8e65d218a90245f5
John Earnest <johnearnest@gmail.com>
2016-01-11 10:32:33 -0700

    addressing minor portability concerns for iKe in Firefox and Chrome, including fixes for audio stuff. Along the way I broke out the audio related code so it's all in one place.

---
+++ ike/audio.js
@@ -0,0 +1,93 @@
+
+// HTML5 Audio realtime synthesis
+
+var SAMPLE_MULT   = 4;
+var sampleCount   = 0;
+var sampleRem     = 0;
+var sampleIndex   = 0;
+var bufferedAudio = [];
+
+function getAudioSample() {
+	// multiply samples out:
+	if (sampleRem == SAMPLE_MULT) { sampleRem = 0; sampleIndex++; }
+	else { sampleRem++; }
+	
+	// fetch more samples from K if we're out of buffered ones:
+	if (sampleIndex >= bufferedAudio.length) {
+		var samples = null;
+		if (env.d["play"].t == 3) {
+			samples = env.d["play"];
+		}
+		else {
+			samples = callk1("play", sampleCount);
+			if (samples.t == 0) { samples = k(3, [samples]); }
+		}
+		sampleCount += len(samples);
+		bufferedAudio = [];
+		sampleIndex = 0;
+		for(var z = 0; z < len(samples); z++) {
+			bufferedAudio.push(samples.v ? samples.v[z].v : 0);
+		}
+	}
+
+	// discharge a sample from our prepared buffer:
+	return bufferedAudio[sampleIndex];
+}
+
+var audio;
+var soundSource;
+var scriptNode;
+
+function audioSetup() {
+	if (audio && soundSource) { return true; }
+	if (!audio) {
+		if (typeof AudioContext !== 'undefined') {
+			audio = new AudioContext();
+		}
+		else if (typeof webkitAudioContext !== 'undefined') {
+			audio = new webkitAudioContext();
+		}
+	}
+	if (audio) {
+		scriptNode  = audio.createScriptProcessor(4096, 1, 1);
+		scriptNode.onaudioprocess = function(audioProcessingEvent) {
+			var inputBuffer  = audioProcessingEvent.inputBuffer;
+			var outputBuffer = audioProcessingEvent.outputBuffer;
+			for(var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+				var inputData  = inputBuffer.getChannelData(channel);
+				var outputData = outputBuffer.getChannelData(channel);
+				if ("play" in env.d) {
+					setvar("srate", num(Math.floor(outputBuffer.sampleRate / SAMPLE_MULT)));
+					for(var sample = 0; sample < inputBuffer.length; sample += 1) {
+						outputData[sample] = getAudioSample();
+					}
+				}
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+function audioPlay() {
+	if (running && audioSetup()) {
+		sampleCount   = 0;
+		sampleRem     = 0;
+		sampleIndex   = 0;
+		bufferedAudio = [];
+		soundSource = audio.createBufferSource();
+		soundSource.buffer = audio.createBuffer(1, 4096, audio.sampleRate);
+		soundSource.connect(scriptNode);
+		scriptNode.connect(audio.destination);
+		soundSource.loop = true;
+		soundSource.start(0);
+	}
+}
+
+function audioStop() {
+	if (soundSource != null) {
+		scriptNode.disconnect();
+		soundSource.stop(0);
+		soundSource.disconnect();
+	}
+}
